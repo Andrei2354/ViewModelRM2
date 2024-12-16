f1d9789eeea4f5bce1d4a2b1515bb513c409d02b
Andrei2354 <andreiaimar1362@gmail.com>
2024-12-16 13:49:01 +0000

    probando diferentes pantallas

--- app/src/main/java/com/example/viewmodelrm/view/TaskApp.kt
+++ app/src/main/java/com/example/viewmodelrm/view/TaskApp.kt
@@ -15,7 +15,7 @@ fun AppNavigation(
 ) {
     NavHost(
         navController = navController,
-        startDestination = "mapa",
+        startDestination = "crud",
         modifier = modifier
     ) {
         composable("mapa") {
