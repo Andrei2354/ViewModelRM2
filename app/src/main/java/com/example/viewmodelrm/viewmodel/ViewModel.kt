b40066851812bdddff7468749ff734dbd6e5dea5
Andrei2354 <andreiaimar1362@gmail.com>
2024-12-16 13:45:39 +0000

    Añadido funciones en el viermodel de insertar, actualizar y eleminar

--- app/src/main/java/com/example/viewmodelrm/viewmodel/ViewModel.kt
+++ app/src/main/java/com/example/viewmodelrm/viewmodel/ViewModel.kt
@@ -5,6 +5,7 @@ import com.example.viewmodelrm.data.TaskDao
 import com.example.viewmodelrm.data.GrupoMarcador
 import com.example.viewmodelrm.data.ValoracionPlaya
 import kotlinx.coroutines.flow.Flow
+import com.example.viewmodelrm.data.Valoracion
 
 class MarcadorViewModel(private val taskdao: TaskDao) : ViewModel() {
 
@@ -14,5 +15,15 @@ class MarcadorViewModel(private val taskdao: TaskDao) : ViewModel() {
     val valoracionPlaya: Flow<List<ValoracionPlaya>> =
         taskdao.getAllvaloracionPlaya()
 
+    suspend fun insertValoracion(valoracion: Valoracion) {
+        taskdao.insertValo(valoracion)
+    }
 
+    suspend fun updateValoracion(valoracion: Valoracion) {
+        taskdao.updateValoracion(valoracion)
+    }
+
+    suspend fun deleteValoracion(valoracion: Valoracion) {
+        taskdao.deleteValoracion(valoracion)
+    }
 }
