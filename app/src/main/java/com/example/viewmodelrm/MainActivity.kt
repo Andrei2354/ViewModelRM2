1ded371b4252115342f0db21899a2bc1f09fcc82
Andrei2354 <andreiaimar1362@gmail.com>
2025-02-19 09:46:27 +0000

    Eliminado cosas que no se usan

--- app/src/main/java/com/example/viewmodelrm/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelrm/MainActivity.kt
@@ -3,25 +3,17 @@ package com.example.viewmodelrm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.viewmodelrm.data.AppDatabase
 import com.example.viewmodelrm.ui.theme.ViewModelRMTheme
-import androidx.compose.foundation.layout.padding
 import com.example.viewmodelrm.viewmodel.MarcadorViewModel
 import com.example.viewmodelrm.viewmodel.ViewModelFactory
-import androidx.navigation.compose.rememberNavController
 import com.example.viewmodelrm.view.PantallaApp
-//import com.example.viewmodelrm.view.MainView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            val navController = rememberNavController()
             val database = AppDatabase.getDatabase(this)
             val taskdao = database.taskDao()
             val viewModel: MarcadorViewModel = viewModel(factory = ViewModelFactory(taskdao))
